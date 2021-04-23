767ae0781edc61103ad539a479ed5cbd6ecab289
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2021-04-23 19:37:48 -0300

    aula03

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+// Por "logo.svg" estar na pasta "public", o caminho irá dar certo
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export default function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    }); 
+
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="Podcastr"/>
+
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>{currentDate}</span>
+        </header>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,159 @@
+// SPA
+// SSR --> Use para páginas muito dinâmicas (getSideServerProps())
+// SSG --> Use para páginas muito estáticas: vai armazenar um cache da página depois do primeiro acesso. Proxy
+// getStaticProps()
+
+import { GetStaticProps } from 'next'; 
+import { api } from '../services/api';
+import { format, parseISO } from 'date-fns'; 
+import ptBR from 'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import styles from './home.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+type Episode = {
+  id: string; 
+  title: string;
+  thumbnail: string;
+  members: string;
+  duration: string;
+  durationAsString: string;
+  url: string; 
+  publishedAt: string; 
+
+}
+
+type HomeProps = {
+  latestEpisodes: Episode[],
+  allEpisodes: Episode[],
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  return (
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos Lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <Image
+                width={192}
+                height={192}
+                src={episode.thumbnail} 
+                alt={episode.title}
+                objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio"/>
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+          <h2>Todos episódios</h2>
+
+          <table cellSpacing={0}>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Podcast</th>
+                  <th>Integrantes</th>
+                  <th>Data</th>
+                  <th>Duração</th>
+                  <th></th>
+                </tr>  
+              </thead>
+
+              <tbody>
+                {allEpisodes.map(episode => {
+                  return (
+                    <tr key={episode.id}>
+                      <td style={{ width: 72 }}>
+                        <Image 
+                          width={120}
+                          height={120}
+                          src={episode.thumbnail}
+                          alt={episode.title}
+                          objectFit="cover"
+                        />
+                      </td>
+                      <td>
+                      <Link href={`episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                        </Link>
+                      </td>
+                      <td>{episode.members}</td>
+                      <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                      <td>{episode.durationAsString}</td>
+                      <td>
+                        <button type="button">
+                          <img src="/play-green.svg" alt="Tocar episódio"/>
+                        </button>
+                      </td>
+                    </tr>
+                  )
+                })}  
+              </tbody>    
+          </table>
+      </section>
+    </div>
+  );
+}
+
+
+// Habilitar o SSR -> requisição feita no NodeJS que está no Next.js por baixo dos panos.
+// Essa requisição é feita antes do cliente carregar a página
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc"
+    }
+  })
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title, 
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      url: episode.file.url,
+    };
+  })
+
+  const latestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.length)
+
+  // props sempre deve ter props
+  return {
+    props: {
+      latestEpisodes,
+      allEpisodes,
+    },
+
+    revalidate: 60 * 60 * 8, // intervalo de tempo para gerar um novo cache
+  };
+}
+
+// getEffect(() => {fetch API, for example. What you're going to do}, [variable that you want to listen to])
